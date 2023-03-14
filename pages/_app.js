44a92793f2d32ff06fd1da1a4323825e5672de61
Karel Kvítek <karel.kvitek@seznam.cz>
2023-03-14 08:58:35 +0100

    initial cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+// Next.js - Google fonts
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600"],
+  subsets: ["latin-ext"],
+  display: "swap",
+  style: ["normal", "italic"],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${poppins.className} app`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
